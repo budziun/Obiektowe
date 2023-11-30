d992de4c3f5ea7ca9345d0c8dad38584c0d9341b
Jakub Budzich <115219579+budziun@users.noreply.github.com>
2023-11-30 15:18:49 +0100

    Add files via upload

--- lab07_file/Car.java
+++ lab07_file/Car.java
@@ -1,6 +1,6 @@
 public record Car(String Brand,String Model,double fuelConsumptionPer100km) {
     public void fuelCost(double fuelPrice, double distance){
-        double cena= (distance/100)*fuelPrice*distance;
+        double cena= (distance/100)*fuelPrice*fuelConsumptionPer100km;
         System.out.println(cena);
     }
 }
